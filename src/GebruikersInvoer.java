154c3ab62f81653edb20ad844c36d4946edded52
CorinneSpruit <corinnemeijdam@hotmail.com>
2020-09-24 14:11:05 +0200

    Nu ook met de tekst erbij 'Voer een woord in' en 'het woord heeft x letters'

--- src/GebruikersInvoer.java
+++ src/GebruikersInvoer.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class GebruikersInvoer {
+
+   public static void main(String [] arg){
+
+System.out.println("Voer een woord in");
+
+          var s = new Scanner(System.in);
+
+      //      var getal = s.nextInt();
+
+          var tekst = s.nextLine();
+
+          System.out.print("Het woord "+tekst+" heeft ");
+          System.out.print(tekst.length());
+          System.out.print(" letters");
+
+
+   }
 }
